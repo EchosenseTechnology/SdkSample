0353f73bc5b0dfd7a100febbec122759046f7175
Nikhil Gupta <nikhil@echosense.in>
2020-02-19 22:37:07 +0530

    Updated sdk version to 3.0.1 and adUnit to 2.0.1 Usage of OfferWall display

--- app/src/main/java/in/echosense/sample/MainActivity.java
+++ app/src/main/java/in/echosense/sample/MainActivity.java
@@ -3,7 +3,14 @@ package in.echosense.sample;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import in.echosense.echosdk.EchoSdk;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +19,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             Intent reqPerm = new Intent(this, AppPermissions.class);
             reqPerm.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(reqPerm);
         }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //display offer
+                EchoSdk.Initialize().with(MainActivity.this).DisplayOfferWall();
+            }
+        });
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.open_offer_wall:
+                //display offer
+                EchoSdk.Initialize().with(MainActivity.this).DisplayOfferWall();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
 }
